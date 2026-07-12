a3b931958ea695b2e708310d495e34f81eba9733
Kouassi Sylvester <sylvesterkouassi9@gmail.com>
2026-07-12 11:21:35 +0100

    initialize Spring Boot project

---
+++ src/main/java/com/objectstorage/backend/ObjectStorageBackendApplication.java
@@ -0,0 +1,15 @@
+package com.objectstorage.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ObjectStorageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectStorageBackendApplication.class, args);
+	}
+
+}
